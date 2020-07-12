9bf0a2aa2f82561aca525a9f44ab076c403d51fc
clement0010 <clement0010@hotmail.com>
2020-07-12 18:06:39 +0800

    first commit

---
+++ src/components/todoComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="todo">
+    <h1>Todo List</h1>
+    <input placeholder="Add a todo" v-model="title" @keypress.enter="addTodo" />
+    <ul v-for="(todo, index) in todoList" :key="index">
+      <li>{{ todo.id }}. {{ todo.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        todoList: [],
+        id: null,
+        title: '',
+      };
+    },
+    // Load data with mounted
+
+    methods: {
+      addTodo: function() {
+        this.id = ++this.id;
+        this.todoList.push({
+          id: this.id,
+          isComplete: false,
+          title: this.title,
+        });
+        this.title = '';
+      },
+    },
+  };
+</script>
